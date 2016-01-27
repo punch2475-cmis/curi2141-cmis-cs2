e0c3ec04252e4f5541649806f93fa1414e4a9fd2
cmis <cmis@cmis-hs21.(none)>
2016-01-27 11:12:50 +0700

    I did stuff

---
+++ hello.py
@@ -0,0 +1,28 @@
+print "Hello, World!"
+print "I am Curi"
+print "I am next to Punch"
+print "Her nickname is Sadness"
+print "She calls me Disgust"
+print "She is dressed as a piglet"
+print "We just had aerobics together"
+print "Our legs hurt"
+print "We are tired"
+print "We want ICE CREAM!!"
+print "Sadness it taking my warm jacket"
+print "Sadness is sad because Disgust is disgusted by Sadness"
+print "Sadness is usually blue but today she is pink"
+print "Sadness says,"
+print "One team always loses. That is sad"
+print "Disgust is green"
+print "Disgust is disgusted by a brocholi"
+print "Bye bye!"
+print True
+print False
+print ("Youbeen", "Curi", "Ko", "Nickname:Disgust", "Age:15")
+print 132235364567
+print 232424.356
+print -2.33232
+print ("Punch", "Nickname:Sadness", "14")
+print ("Biology", "Forensic Science", "Chemistry")
+print ("Algebra 1", "Algebra 2", "precalculous", "AP Calculous")
+print "tired"
