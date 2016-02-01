c099a3abede4420b6df35ce4882c5aee652ded15
cmis <cmis@cmis-hs21.(none)>
2016-02-01 09:55:30 +0700

    Assignment 1

---
+++ assignment1.py
@@ -0,0 +1,26 @@
+MyName = "Curi"
+MyAgeInYears = 15.4
+MyHeightInM = 1.59
+lofsquare = 6
+lofrectangle = 10
+hofsquare = 4
+nofmonths = 12
+AgeInMonths = MyAgeInYears * nofmonths
+print AgeInMonths
+lifeExpectancy = 80.04
+LeftToLive = lifeExpectancy - MyAgeInYears
+print LeftToLive
+MtoFT = 3.2808
+MyHeightInFeet = MyHeightInM * MtoFT
+print MyHeightInFeet
+AverageFemaleHeight = 1.584960
+DifferenceFromAverage = MyHeightInM - AverageFemaleHeight
+print DifferenceFromAverage
+AreaofSquare = lofsquare * lofsquare
+print AreaofSquare
+VolumeofSquare = AreaofSquare * hofsquare
+HalfofVolume = VolumeofSquare / 2
+print HalfofVolume
+AreaofRectangle = lofrectangle * hofrectangle 
+ninthrectangle = (AreaofRectangle) / (1/9)
+print ninthrectangle
