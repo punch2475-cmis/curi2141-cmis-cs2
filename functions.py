c41b907dd24a45a8ea43460e5e94d81f809eed3e
cmis <cmis@cmis-hs21.(none)>
2016-02-08 11:55:22 +0700

    Functions Assignment

---
+++ functions.py
@@ -0,0 +1,16 @@
+def Addition(x,y):
+    return x+y
+
+z= Addition(3,4)
+print z
+
+def Subtraction(a,b):
+	return a-b
+
+c=Subtraction(5,3)
+print c
+
+def Multipication(d,e)
+	return d*e
+
+c=Multipication(4,4)
