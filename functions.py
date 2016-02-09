7dfdcc839dce810a3ff68ec5cd04b315f296a5e4
cmis <cmis@cmis-hs21.(none)>
2016-02-09 14:04:59 +0700

    Functions Assignment

--- functions.py
+++ functions.py
@@ -10,7 +10,50 @@ def Subtraction(a,b):
 c=Subtraction(5,3)
 print c
 
-def Multipication(d,e)
+def Multipication(d,e):
 	return d*e
 
-c=Multipication(4,4)
+f=Multipication(4,4)
+print f
+
+def Division(g,h):
+	return g/h
+i=Division(2,3)
+
+print i
+
+def hours_from_seconds(seconds):
+	return seconds/3600
+
+print hours_from_seconds(86400)
+
+import math
+def area_of_circle(r):
+    a = r**2*math.pi
+    return a
+
+print area_of_circle(1)
+
+def volume_of_sphere(r):
+    v = 4.0/3.0 * math.pi*r**3
+    return v
+
+print volume_of_sphere(5)
+
+def avg_volume(a,b):
+    v=((1.0/6.0*math.pi*a**3)+(1.0/6.0*math.pi*b**3))/2
+    return v
+
+print avg_volume(10, 20)
+    
+def area (a,b,c):
+    s = (a+b+c) /2
+    return (s*(s-a)*(s-b)*(s-c))**0.5
+
+print area (1,2,2.5)
+
+def right_align("word")
+    w=80-int(len(word)+2),"word"
+    
+def msg_box()
+
