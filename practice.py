900e9c02137050f0cc018065722c78407f8bc540
cmis <cmis@cmis-hs21.(none)>
2016-02-17 09:55:29 +0700

    practice

---
+++ practice.py
@@ -0,0 +1,27 @@
+name=raw_input("Type your name>>> ")
+a=raw_input("Type your age>>> ")
+b=raw_input("Type your favorite number that is not 1>>> ")
+c = int(a)*int(b)
+
+output="""
+Guess what {} !
+You are older than you think you are
+You are {} years old !
+""".format(name, c)
+
+print output
+
+food=raw_input("What is your favorite food! >>> ")
+company=raw_input("Type the name of the company that makes your favorite food >>> ")
+
+output="""
+From {}
+Sorry we are not making and selling {} anymore
+""".format(company,food)
+
+print output
+
+def add(x, y):
+	return x + y
+
+def output(name, x, )
