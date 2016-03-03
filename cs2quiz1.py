3d12231d318f1a2b74e14904efa4b1b0931b9f5f
cmis <cmis@cmis-hs12.(none)>
2016-03-03 11:55:04 +0700

    Added scores

--- cs2quiz1.py
+++ cs2quiz1.py
@@ -2,18 +2,21 @@
 #1 1pt) What is the symbol "=" used for?
 #       The "=" symbol is called the assignment operator and its job is to assign a value to a variable
 #
+#1/1
 #
 #2 3pts) Write a technical definition for 'function'
 #   A function is a named sequence of statements that performs a computation.
 #
+#3/3
 #
 #3 1pt) What does the keyword "return" do?
 #   The return keyword does the job of spitting out the value. 
 #
+#1/1
 #
 #4 5pts) We know 5 basic data types. Write the name for each one and provide two
 #   examples of each below
-#	1: Integer
+#	1: Integer 
 #	2: String
 #	3: Boolean
 #	4: Tuple
@@ -24,12 +27,15 @@
 #   Function definition is the process of defining a function, specifies the parameters that are to be inluded in the function and structures the blocks of code. Defining the function makes the function available in the program. 
 #    Function call is a command that executes the defined function with the given arguments
 #
+# 2/2
 #
 #6 3pts) What are the 3 phases that every computer program has? What happens in
 #        each of them
 #	1:Input : In the input part, some value is inserted to the program
 #	2:Processing:  In the processing part, the function does the calculation with the give input
 #	3:Output : In the output part, the function gives an output or the end result of the processing. 
+#
+# 3/3
 #
 #Part 2: Programming (25 points)
 #Write a program that asks the user for the areas of 3 circles.
@@ -45,20 +51,40 @@
 
 # Hint: Radius is the square root of the area divided by pi
 
+#1 pt for header line        1
+#3 pt for correct formula    1
+#1 pt for return value       1
+#1 pt for parameter name     0
+#1 pt for function name      1
+
 import math
 def diameter(x):
     return 2*(math.sqrt(x/math.pi))
 def total_diameter(e,f,g):
     return e+f+g
 
+#1pt for header line                      1
+#1pt for parameter names                  0
+#1pt for return value                     1
+#1pt for correct output format            1
+#3pt for correct use of format function   3
+
 def output(a,b,c,d):
     out="""
+Circle   Diameter
 c1: {}
 c2: {}
 c3: {}
 Totals: {}
 """.format(a,b,c,d)
     return out
+
+#1pt header line                   1
+#1pt getting input                 1
+#1pt converting input              1
+#1pt for calling output function   1
+#2pt for correct diameter formula  2
+#1pt for variable names            0
 
 def main():
     C1=raw_input("Area of C1: ")
