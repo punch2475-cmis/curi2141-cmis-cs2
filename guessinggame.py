2f984b375dcb26017b40f061252520e762d34f82
cmis <cmis@cmis-hs21.(none)>
2016-03-10 14:35:29 +0700

    Dumb Game

---
+++ guessinggame.py
@@ -0,0 +1,12 @@
+
+
+def guess():
+	minimum=raw_input("What is the minimum value? >>> ") 
+    maximum=raw_input("What is the maximum value? >>> ")
+    random=random.randit(minimum, maximum)
+    print "I am thinking of a number between" minimum "and" maximum 
+    guessnumber=raw_input("What do you think it is? >>> ")
+
+    if guessnumber=random
+        msg= "The target was" random
+        msg= "Your guess was" guessnumber
