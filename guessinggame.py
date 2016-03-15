13dcd6e75af175ee4cdc7619610a3eccf4d1ef0c
cmis <cmis@cmis-hs21.(none)>
2016-03-15 09:56:14 +0700

    GAME ASSIGNMENT

--- guessinggame.py
+++ guessinggame.py
@@ -1,12 +1,38 @@
-
-
 def guess():
-	minimum=raw_input("What is the minimum value? >>> ") 
-    maximum=raw_input("What is the maximum value? >>> ")
-    random=random.randit(minimum, maximum)
-    print "I am thinking of a number between" minimum "and" maximum 
-    guessnumber=raw_input("What do you think it is? >>> ")
+	import random
+	minimum = int(raw_input("What is the minimum value? >>>"))
+	maximum = int(raw_input("What is the maximum value? >>>"))
+	random = random.randint(minimum, maximum)
+	print "I am thinking of a number between", minimum, "and", maximum
+	guessnumber = str(raw_input("What do you think it is? >>> "))
+	difference = abs(guessnumber - random)
+	if guessnumber == random:
+		return True
+	elif guessnumber > random:
+		return over False
+	elif guessnumber < random:
+		return under False
 
-    if guessnumber=random
-        msg= "The target was" random
-        msg= "Your guess was" guessnumber
+guess()
+
+def correct():
+	if guess() == True
+		output = """
+		The target was {}
+		Your guess was {}
+		That is correct! You must be a phychic!
+		""".format(random, guessnumber)
+	elif guess() == over False
+		output = """
+		The target was {}
+		Your guess was {}
+		That's over by {}
+		""".format(random, guessnumber, difference)
+	elif guess() == under False
+		output = """
+		The target was {}
+		Your guess was {}
+		That's under by {}
+		""".format(random, guessnumber, difference)
+correct()
+
