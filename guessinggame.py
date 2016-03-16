2b5406d6b0068f45386d21a2290eca1b822045a9
cmis <cmis@cmis-hs21.(none)>
2016-03-16 11:21:27 +0700

    GUESSING GAME

--- guessinggame.py
+++ guessinggame.py
@@ -4,35 +4,26 @@ def guess():
 	maximum = int(raw_input("What is the maximum value? >>>"))
 	random = random.randint(minimum, maximum)
 	print "I am thinking of a number between", minimum, "and", maximum
-	guessnumber = str(raw_input("What do you think it is? >>> "))
+	guessnumber = int(raw_input("What do you think it is? >>> "))
 	difference = abs(guessnumber - random)
+
 	if guessnumber == random:
-		return True
+		print """
+The target was {}
+Your guess was {}
+That is correct! You must be a phychic!
+""".format(random, guessnumber)
 	elif guessnumber > random:
-		return over False
+		print """
+The target was {}
+Your guess was {}
+That's over by {}
+""".format(random, guessnumber, difference)
 	elif guessnumber < random:
-		return under False
-
+		print """
+The target was {}
+Your guess was {}
+That's under by {}
+""".format(random, guessnumber, difference)
+	
 guess()
-
-def correct():
-	if guess() == True
-		output = """
-		The target was {}
-		Your guess was {}
-		That is correct! You must be a phychic!
-		""".format(random, guessnumber)
-	elif guess() == over False
-		output = """
-		The target was {}
-		Your guess was {}
-		That's over by {}
-		""".format(random, guessnumber, difference)
-	elif guess() == under False
-		output = """
-		The target was {}
-		Your guess was {}
-		That's under by {}
-		""".format(random, guessnumber, difference)
-correct()
-
