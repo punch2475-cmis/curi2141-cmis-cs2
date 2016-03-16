266c560e015710b6be3c71fb3f7491d61f3207c3
cmis <cmis@cmis-hs21.(none)>
2016-03-16 11:55:35 +0700

    Story

---
+++ conditionals.py
@@ -0,0 +1,5 @@
+# My script will give a short story of one's future life from information gathered from the individual.
+
+def yourstory()
+    print """
+Once upon a time there was a {} named {}. He/She is a {rich/poor} man/woman. You will have a {adj} {husband/wife} and {} children
