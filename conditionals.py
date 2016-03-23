7fdef062abd16829fbef753d17f8fcb8aaf84267
cmis <cmis@cmis-hs15.(none)>
2016-03-23 11:55:30 +0700

    Assignment

--- conditionals.py
+++ conditionals.py
@@ -1,10 +1,28 @@
-# My script will give them life story of an individual with the information gathered from the user. The variable "name" contains the name of the user. The "gender" variable contains either M which symbolizes male and F which symbolizing female. "MoneyInPocket" variable contains the information on how much money they have. This variable determines whether the individual is going to be rich or poor in the future. "HowAreYou" variable contains the information on whether they are happy or sad. This variable determines whether you are going to have a happy or a sad life with your family in the future. All these variables are inserted into the story and fills in the blanks. 
+# My script will give them a fairytale of an individual with the information gathered from the user. The variable "name" contains the name of the user. The "gender" variable contains either M which symbolizes male and F which symbolizing female. "MoneyInPocket" variable contains the information on how much money they have. This variable determines whether the individual is going to be rich or poor in the future. "HowAreYou" variable contains the information on whether they are happy or sad. This variable determines whether you are going to have a happy or a sad life with your family in the future. All these variables are inserted into the story and fills in the blanks. 
 
 def yourstory():
+    import random
     print """
-Once upon a time there was a {} named {}. He/She was {} his/her entire life. He/She lived with a {adj} husband/wife and {} children. The {} had a {} life with your family. He/She had {} chance of winning lottery. {interesting facts such as secretly being a prince or being arrested.}
-""".format(gender, name, MoneyInPocket, ) #job using random.random , # random.randit for adj of husband/wife
+Once upon a time there was a {} named {}. He/She was {} his/her entire life. He/She lived with a {} husband/wife and {} children. He/She had a {} life with your family. He/She had {} chance of winning lottery. {interesting facts such as secretly being a prince or being arrested.}
+""".format(gender, name, MoneyInPocket, RandomRandint, RandomRandint, HowAreYou,  ) #job using random.random , # random.randit for adj of husband/wife
+def lottery():
+    answer=random.randint(1,3)
+    print "I am thinking of a number between 1 and 3 and it is not a decimal."
+    guessnumber = int(raw_input("What do you think it is? >>> "))
 
+def SecretOfBirth():
+    print "Now lets look at your secrets! Type a,b or c."
+    Choice="""
+a. I have a dead body in my basement
+b. I am a maniac of something that is nasty
+c. I am secretly a mermaid
+"""
+    if Choice == "a":
+        return ""
+    elif Choice == "b":
+        
+
+        
 def information():
 	import random
 	name=raw_input("What is your name? >>> ")
@@ -14,7 +32,10 @@ def information():
 	NumberOfSiblings=raw_input("How many siblings do you have? >>> ")
 	HowAreYou=raw_input("Are you happy or sad? >>> ")
 	RandomRandint=random.randint(len(name), Coins)
-    
+    RandomRandom=random.random
+    lottery()
+    SecretOfBirth()
+
 	#gender
 	if gender == "M":
 		print "man"
@@ -30,6 +51,7 @@ def information():
 
 	elif MoneyInPocket < len(name):
 		print "had no money"
+
 	#Adj for husband/wife (random.randint)
     if RandomRandint > (len(name)+Coins)/2
         print "hairy"
@@ -45,6 +67,15 @@ def information():
 
     elif RandomRandint < (len(name)-Coins)
         print "rich"
+    #number of children
+    if RandomRandom > 0.5
+        print "beggar"
+
+    elif RandomRandom < 0.5
+        print "doctor"
+
+    elif RandomRadom == 0.5
+        print "photographer"
 
 	#Type of family
 	if HowAreYou == "happy":
@@ -52,4 +83,13 @@ def information():
 	
 	else:
 		print "happy"
+    #lottery
+    if guessnumber == random
+        print "He/She won the lottery at least 3 times in her life time!"
+    elif guess number < random
+        print "He/She didn't even have one chance winning the lottery no matter how many times he/she tried because luck was just never on his/her side."
+    elif guess number > random
+        print "He/She could have won a lottery but didn't try."
 
+    #Discovery
+    
