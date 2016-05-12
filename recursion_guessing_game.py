e265ccfa88d1db5434b71aeffd57a75f73502765
cmis <cmis@cmis-hs15.(none)>
2016-05-12 11:55:32 +0700

    assignment

--- recursion_guessing_game.py
+++ recursion_guessing_game.py
@@ -14,7 +14,7 @@ def game(targetnumber, tries):
 		print "It is too low"
 		game(targetnumber, tries-1)
 
-def rounds(tries, num_of_rounds):
+def rounds(tries, num_of_rounds, ):
 	import random
 	targetnumber = int(random.randint(1, 100))
 
@@ -26,9 +26,24 @@ def rounds(tries, num_of_rounds):
 		print "Round", num_of_rounds -1
 		rounds(tries, num_of_rounds -1)
 
+def bonus(first_half_guess, target):
+
+	if first_half_guess > target:
+		print "It is too high"
+		bonus(1/2 * first_half_guess)
+
+	elif first_half_guess == target:
+		print "You are awesome!!"
+
+	elif first_half_guess < target:
+		print "It is too high"
+		bonus((1/2 * first_half_guess) + first_half_guess)
+
 def main():
 	tries = 5
 	num_of_rounds = 3
-	Rounds = rounds(num_of_rounds)
+	Rounds = rounds(tries, num_of_rounds)
 
+	target = raw_input("Target number: ")
+	first_half_guess = 50
 main()
